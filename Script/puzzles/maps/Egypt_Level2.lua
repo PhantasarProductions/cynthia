6d57233d34c623910a419d8076e73a7f9c4fdb84
Tricky1975 <facebook@tbbs.nl>
2017-11-11 01:56:52 +0100

    Some changes here?

--- Script/puzzles/maps/Egypt_Level2.lua
+++ Script/puzzles/maps/Egypt_Level2.lua
@@ -1,4 +1,4 @@
--- Generated on: Fri 10 November 2017; 16:24:29
+-- Generated on: Fri 10 November 2017; 17:31:55
 -- Author:       Jeroen P. Broks
 -- License:      GPL 3
 
@@ -65,14 +65,14 @@ local ret = {}
 			{   1,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0},
 			{   1,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0},
 			{   1,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0},
-			{   1,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0},
+			{   1,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   3,   0},
 			{   1,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0},
 			{   1,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   1,   1,   1,   0,   0,   3,   0,   0,   0,   0,   0,   0,   0},
-			{   1,   0,   0,   0,   0,   1,   1,   1,   0,   0,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   0,   3},
+			{   1,   0,   0,   0,   0,   1,   1,   1,   0,   0,   0,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   0,   3},
 			{   1,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   1,   1,   1},
-			{   0,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0},
+			{   0,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   3,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0},
 			{   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   3,   1,   1,   1,   1,   1,   1},
-			{   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   3,   0,   0,   1,   1,   1,   1,   1,   1,   1,   1,   0,   0}}
+			{   0,   0,   0,   0,   3,   0,   0,   0,   0,   0,   0,   0,   3,   0,   0,   1,   1,   1,   1,   1,   1,   1,   1,   0,   0}}
 
 	ret.objects = {
 			{ {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}},
@@ -92,6 +92,38 @@ local ret = {}
 			{ {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}}}
 
 	ret.fetchteddyobject = {}
+		ret.objects[6][12][#ret.objects[6][12]+1] = {
+			objtype = "Snake",
+			coords = {x=12, y=6},
+			data = {
+				['TeddyID'] = "B-5-1",
+				['Wind'] = "W"}}
+		ret.fetchteddyobject['B-5-1'] = ret.objects[6][12][#ret.objects[6][12]]
+
+		ret.objects[8][8][#ret.objects[8][8]+1] = {
+			objtype = "Snake",
+			coords = {x=8, y=8},
+			data = {
+				['TeddyID'] = "7-7-1",
+				['Wind'] = "E"}}
+		ret.fetchteddyobject['7-7-1'] = ret.objects[8][8][#ret.objects[8][8]]
+
+		ret.objects[10][10][#ret.objects[10][10]+1] = {
+			objtype = "Snake",
+			coords = {x=10, y=10},
+			data = {
+				['TeddyID'] = "9-9-1",
+				['Wind'] = "W"}}
+		ret.fetchteddyobject['9-9-1'] = ret.objects[10][10][#ret.objects[10][10]]
+
+		ret.objects[12][10][#ret.objects[12][10]+1] = {
+			objtype = "Snake",
+			coords = {x=10, y=12},
+			data = {
+				['TeddyID'] = "9-B-1",
+				['Wind'] = "E"}}
+		ret.fetchteddyobject['9-B-1'] = ret.objects[12][10][#ret.objects[12][10]]
+
 		ret.objects[13][2][#ret.objects[13][2]+1] = {
 			objtype = "Start",
 			coords = {x=2, y=13},
@@ -99,6 +131,14 @@ local ret = {}
 				['TeddyID'] = "1-C-1",
 				['Wind'] = "E"}}
 		ret.fetchteddyobject['1-C-1'] = ret.objects[13][2][#ret.objects[13][2]]
+
+		ret.objects[13][20][#ret.objects[13][20]+1] = {
+			objtype = "Snake",
+			coords = {x=20, y=13},
+			data = {
+				['TeddyID'] = "13-C-1",
+				['Wind'] = "W"}}
+		ret.fetchteddyobject['13-C-1'] = ret.objects[13][20][#ret.objects[13][20]]
 
 		ret.objects[13][25][#ret.objects[13][25]+1] = {
 			objtype = "Exit",
