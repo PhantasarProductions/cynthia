ed6823ac06e66c960c9f91a45cf64a9ecbe3e36d
Tricky1975 <facebook@tbbs.nl>
2018-09-04 20:03:16 +0200

    Troll Tutorial

--- Script/ingametutors.lua
+++ Script/ingametutors.lua
@@ -20,7 +20,7 @@
 		
 	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
 	to the project the exceptions are needed for.
-Version: 18.01.24
+Version: 18.09.04
 ]]
 local tutors = {
                      snakes     = "Whenever you get in line with a snake he'll spit poison at you killing you instantly. They cannot turn around, so maybe you can sneak past them",
@@ -30,7 +30,8 @@ local tutors = {
                      pushstone  = "The stone blocks can be either pushed or pulled. Do so to find your way",
                      witch      = "Witches can be helpful, or be bothersome\n\nAs soon as a witch sees you she'll teleport you to a set location.\n\nThe locations are NOT randomly chosen. Each witch has a set spot where she'll always transport you to.\n\nWhile witches can be killed with rocks and daggers, think well if this is the best thing to do.\n\nWitches are the perfect example of how aggression can backfire on you. Although some witches always send you to a spot that works in your disadvantage, some witches can send you to spots you cannot reach otherwise. but which may be needed to get to in order to solve the puzzle.\n\nThink twice before you kill a witch!",
                      faerie     = "Faeries can if you touch them protect you from witches and maybe also from other kinds of attacks. Please note their protection only lasts several actions, and the maximum amount of actions does not stack\n\nNot all faeries give you the same duration of protection. When touching a faerie who grants less protection than you currently have nothing will happen. Only the 'strongest' protection counts.",
-                     colblocks  = "Colored blocks can be pushed and pulled, but if you push two blocks of the same color next to each other they'll disappear.\nFor color-blind people symbols have been put on the blocks.\n\nIf all blocks of a certain color have been removed a barrier may get deactivated. Sometimes you need ALL blocks to disappear before you get the barrier removed.\n\nBeware, some blocks appear in an odd number. These can require to be removed by managing to get a combination of 3 together without getting them to disappear due to having 2 together already."
+                     colblocks  = "Colored blocks can be pushed and pulled, but if you push two blocks of the same color next to each other they'll disappear.\nFor color-blind people symbols have been put on the blocks.\n\nIf all blocks of a certain color have been removed a barrier may get deactivated. Sometimes you need ALL blocks to disappear before you get the barrier removed.\n\nBeware, some blocks appear in an odd number. These can require to be removed by managing to get a combination of 3 together without getting them to disappear due to having 2 together already.",
+                     trolls     = "Trolls will push Cynthia whenever she touches them.\nWhen pushed Cynthia will only come to a standstill when she comes into touch with something blocking her path<br>They can be killed with rocks and daggers, but sometimes trolls can also push you over water and gaps allowing her to reach places she can't reach otherwise.<br>So look well before you kill a troll."
                      
                }
 
