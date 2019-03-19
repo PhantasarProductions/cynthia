b099ecc2dd4959950452800e555e53e55d24a6fa
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-19 16:18:56 +0100

    vUpdate

--- Script/chain_sections.lll/game.lua
+++ Script/chain_sections.lll/game.lua
@@ -1,38 +1,30 @@
 --[[
-	Cynthia Johnson
-	Game Engine
-	
-	
-	
-	(c) Jeroen P. Broks, 2017, 2018, 2019
-	
-		This program is free software: you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-		(at your option) any later version.
-		
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
-		You should have received a copy of the GNU General Public License
-		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	
-	Please note that some references to data like pictures or audio, do not automatically
-	fall under this licenses. Mostly this is noted in the respective files.
-		
-Version: 19.03.01
+Cynthia Johnson
+Main Engine
+
+
+
+(c) Jeroen P. Broks, 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Please note that some references to data like pictures or audio, do not automatically
+fall under this licenses. Mostly this is noted in the respective files.
+
+Version: 19.03.19
 ]]
 
 
---[[
-    Xtra Wall codes
-    256 = Killable enemy
-    257 = Unkillable enemy or general invisible wall
-    258 = Pushable item
-    259 = Pullable item
-    260 = Pushable and pullable item
-]]
 
 local tutor = j_love_import('/SCRIPT/INGAMETUTORS.LUA')
 
