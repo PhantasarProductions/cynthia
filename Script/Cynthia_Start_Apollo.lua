a09b2d3b46d9e7e332bda4d7c6f1d10031f0fd3d
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-07-17 16:18:49 +0200

    Update Cynthia_Start_Apollo.lua

--- Script/Cynthia_Start_Apollo.lua
+++ Script/Cynthia_Start_Apollo.lua
@@ -1,7 +1,214 @@
-builddate    = "Wed 26 June 2019; 15:38:31"
+--builddate    = "Wed 26 June 2019; 15:38:31"
 gametitle    = "Cynthia Johnson"
-buildversion = "19.06.26" 
+--buildversion = "19.06.26" 
 
 hate = Neil.Globals.Hate
 local hate = hate
+
+function j_hate_import(afile,keepcase)
+	return hate.import("",afile)
+end
+
+-- Boolean features
+boolyn = { [true] = "yes", [false] =  "no" }
+boolbt = { [true] =     1, [false] =     0 }
+boolon = { [true] =  "on", [false] = "off" }
+
+
+each   = Neil.Globals.Each
+ieach  = Neil.Globals.Inv_Each
+spairs = Neil.Globals.sPairs
+
+function valstr(a)
+return ({
+   ['nil'] = function(a) return 'nil' end,
+   ['number'] = function(a) return ''..a end,
+   ['string'] = function(a) return a end,
+   ['boolean'] = function(a) return ({[true]='true', [false]='false'})[a] end,
+   ['function'] = function(a) return("valstr does not support functions") end,
+   ['table'] = function(a) return('Valstr does not support tables') end})[type(a)](a)
+end
+
+strval = valstr
+
+
+
+--[[
+  
+  This function was written by Wookai
+  http://stackoverflow.com/questions/2282444/how-to-check-if-a-table-contains-an-element-in-lua
+  
+]]
+function tablecontains(table, element)
+  for _, value in pairs(table) do
+    if value == element then
+      return true
+    end
+  end
+  return false
+end
+
+
+
+
+
+function isorcontains(v,e)
+if type(v)=="table" then return tablecontains(v,e) end
+return v==e
+end     
+
+--[[ The name of the person who came up with this is unknown,
+      however he placed this script here:
+      
+      http://stackoverflow.com/questions/1426954/split-string-in-lua
+      
+]]      
+
+function mysplit(inputstr, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t={} ; i=1
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                t[i] = str
+                i = i + 1
+        end
+        return t
+end
+
+function len(a)
+local k,v
+local ret=0
+if type(a)=="table" then
+  --for k,v in ipairs(a) do
+  --    ret = ret + 1
+  --    end
+  return #a
+  end
+return string.len(a.."") -- the .."" is to make sure this is string formatted! ;)  
+end
+
+
+-- String features --
+upper = string.upper
+lower = string.lower
+chr = string.char
+printf = string.format
+replace = string.gsub
+rep = string.rep
+substr = string.sub
+
+
+left  = Neil.Globals.Left
+right = Neil.Globals.Right
+mid   = Neil.Globals.Mid
+
+function trim(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+
+function findstuff(haystack,needle) -- BLD: Returns the position on which a substring (needle) is found inside a string or (array)table (haystrack). If nothing if found it will return nil.<p>Needle must be a string if haystack is a string, if haystack is a table, needle can be any type.
+local ret = nil
+local i
+for i=1,len(haystack) do
+    if type(haystack)=='table'  and needle==haystack[i] then ret = ret or i end
+    if type(haystack)=='string' and needle==mid(haystack,i,len(needle)) then ret = ret or i end
+    -- rint("finding needle: "..needle) if ret then print("found at: "..ret) end print("= Checking: "..i.. " >> "..mid(haystack,i,len(needle)))
+    end
+return ret    
+end
+
+function safestring(s)
+local allowed = "qwertyuiopasdfghjklzxcvbnmmQWERTYUIOPASDFGHJKLZXCVBNM 12345678890-_=+!@#$%^&*():;/<>[]{}.,"
+local i
+local safe = true
+local alt = ""
+for i=1,len(s) do
+    safe = safe and (findstuff(allowed,mid(s,i,1))~=nil)
+    alt = alt .."\\"..string.byte(mid(s,i,1),1)
+    end
+-- print("DEBUG: Testing string"); if safe then print("The string "..s.." was safe") else print("The string "..s.." was not safe and was reformed to: "..alt) end    
+local ret = { [true] = s, [false]=alt }
+-- print("returning "..ret[safe])
+return ret[safe]     
+end 
+
+-- Serializing
+function TRUE_SERIALIZE(vname,vvalue,tabs,noenter)
+local ret = ""
+local work = {
+                ["nil"]        = function() return "nil" end,
+                ["number"]     = function() return vvalue end,
+                ["function"]   = function() return "'!ERROR! -- I cannot handle functions!'" end,
+                ["string"]     = function() return "\""..safestring(vvalue).."\"" end,
+                ["boolean"]    = function() return ({[true]="true",[false]="false"})[vvalue] end,
+                ["table"]      = function()
+                                 local titype
+                                 local tindex = {
+                                                   ["number"]     = function(v) return v end,
+                                                   ["boolean"]    = function(v) return ({[true]="true",[false]="false"})[v] end,
+                                                   ["string"]     = function(v) return "\""..safestring(v).."\"" end
+                                 }
+                                 local wrongindex = function() print("!ERROR! Type "..titype.." can not be used as a table in serializing") return "!WRONGINDEX" end
+                                 local ret = "{"
+                                 local k,v
+                                 local result
+                                 local notfirst
+                                 for k,v in pairs(vvalue) do
+                                     if notfirst then ret = ret .. ",\n" else notfirst=true ret = ret .."\n" end
+                                     titype = type(k)
+                                     result = (tindex[titype] or wrongindex)(k)
+                                     -- print(titype.."/"..k)
+                                     ret = ret .. TRUE_SERIALIZE("["..result.."]",v,(tabs or 0)+1,true)
+                                     end
+                                 if notfirst then    
+                                   ret = ret .."\n"    
+                                   for i=1,(tabs or 0) do ret = ret .."\t" end   
+                                   for i=1,len(vname.." = ") do ret = ret .. " " end
+                                   end 
+                                 ret = ret .. "}"  
+                                 return ret  
+                                 end 
+                                   
+             }             
+local letsgo = work[type(vvalue)] or function() print("!ERROR! Unknown type. Cannot serialize","Variable,"..vname..";Type Value,"..type(vvalue)) return "whatever" end
+local i
+for i=1,(tabs or 0) do ret = ret .."\t" end
+if vname then 
+   ret = ret .. vname .." = "..letsgo()
+   else
+   ret = letsgo()
+   end 
+if not noenter then ret = ret .."\n" end
+return ret
+end
+
+
+function serialize(vname,variableitself)
+local ret = ""
+local v = variableitself 
+if vname then 
+   v = v or _G[vname] 
+   if type(vname)~='string' then print("First variable must be the name to return in the serializer string") end
+   end
+ret = TRUE_SERIALIZE(vname,v)
+-- JBCSYSTEM.Returner(ret)
+return ret
+end
+
+function sval(a)
+return 
+  (({
+     ['string']=function() return a end,
+     ['number']=function() return a end,
+     ['boolean']=function() if a then return 'true' else return 'false' end     end
+  })[type(a)] or function() return "<< "..type(a).." >>" end)()
+end  
+
+
+
+
+--j_love_import("SCRIPT/ILOVEYOU.LUA")
+hate.import("Script","iHateYou.lua")
 
