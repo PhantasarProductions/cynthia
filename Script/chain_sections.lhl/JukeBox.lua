6bfa3ef93c1196a559261831cadac059d687c8ae
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-07-18 18:17:11 +0200

    I hate you
    Plus removed dupe license

--- Script/chain_sections.lhl/JukeBox.lua
+++ Script/chain_sections.lhl/JukeBox.lua
@@ -4,7 +4,7 @@
 -- 
 -- 
 -- 
--- (c) Jeroen P. Broks, 1, 2022
+-- (c) Jeroen P. Broks, 2017, 2022
 -- 
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -23,31 +23,6 @@
 -- 
 -- Version: 22.07.17
 -- </License Block>
---[[
-Cynthia Johnson
-Jukebox
-
-
-
-(c) Jeroen P. Broks, 
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-Please note that some references to data like pictures or audio, do not automatically
-fall under this licenses. Mostly this is noted in the respective files.
-
-Version: 19.06.26
-]]
 
 
 
@@ -56,7 +31,7 @@ local jb = {}
 local lunar = lunar
 local luna = luna
 
-jb.data = j_love_import("SCRIPT/JUKEBOXDATA.LUA")
+jb.data = j_hate_import("SCRIPT/JUKEBOXDATA.LUA")
 
 jb.gui = {
              kind='picture',
