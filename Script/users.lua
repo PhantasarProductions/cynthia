f7cce2aaf901e2407f9bfd8f098524b676898084
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-07-19 19:06:58 +0200

    I hate you

--- Script/users.lua
+++ Script/users.lua
@@ -21,36 +21,13 @@
 -- Please note that some references to data like pictures or audio, do not automatically
 -- fall under this licenses. Mostly this is noted in the respective files.
 -- 
--- Version: 22.07.17
+-- Version: 22.07.19
 -- </License Block>
---[[
-	Cynthia Johnson
-	User loader
-	
-	
-	
-	(c) Jeroen P. Broks, 2017, 2018, All rights reserved
-	
-		This program is free software: you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-		(at your option) any later version.
-		
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
-		You should have received a copy of the GNU General Public License
-		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-		
-	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
-	to the project the exceptions are needed for.
-Version: 18.01.24
-]]
+
 
 -- *import annaandgamejolt
 
-mkl.version("Cynthia Johnson (for Apollo) - users.lua","22.07.17")
+mkl.version("Cynthia Johnson (for Apollo) - users.lua","22.07.19")
 mkl.lic    ("Cynthia Johnson (for Apollo) - users.lua","GNU General Public License 3")
 
 --mynet = GAHD_get("CYNTHIA.GHD")
@@ -60,10 +37,10 @@ function login(uname)
     local f = { [false]='failed',[true]='success'}
     -- Load user
     local ufile = "users/"..uname..".lua"
-    assert(love.filesystem.isFile(ufile),"User "..ufile.." does not exist")
-    user  = j_love_import(ufile)
+    assert(hate.filesystem.isFile(ufile),"User "..ufile.." does not exist")
+    user  = j_hate_import(ufile)
     -- Set to startup default
-    love.filesystem.write("config/autologin.lua","return '"..uname.."'")
+    hate.filesystem.write("config/autologin.lua","return '"..uname.."'")
     -- Login: Anna
     -- Login: Game Jolt
     user.username=uname
@@ -75,10 +52,10 @@ function login(uname)
       for k,d in spairs(breakdown) do msg = msg..k.. ":"..f[breakdown[k].success~=nil and breakdown[k].success~=false] .. "\n"..(breakdown[k].reason or 'No reason received').."\n\n\n" end
       ok = net 
       if not ok then
-         local  a =  love.window.showMessageBox( "Network login failure", msg, {[1]='Retry',[2]='Ignore',[3]='Quit',escapebutton=3} )
+         local  a =  hate.window.showMessageBox( "Network login failure", msg, {[1]='Retry',[2]='Ignore',[3]='Quit',escapebutton=3} )
          if     a==1 then ok=false; print("Retrying")
          elseif a==2 then ok=true;  print("Ignoring")
-         else             forcequit=true love.event.quit() ok=true; print("Quitting") end
+         else             forcequit=true hate.event.quit() ok=true; print("Quitting") end
       end   
     until ok  
 end
@@ -87,9 +64,9 @@ function saveuser()
     local uname = user.username    
     local ufile = "users/"..uname..".lua"
     user.username=nil
-    local success, message = love.filesystem.write( ufile, serialize("ret",data).."\n\nreturn ret\n\n"  )
+    local success, message = hate.filesystem.write( ufile, serialize("ret",data).."\n\nreturn ret\n\n"  )
     user.username=uname
     if not success then
-       love.window.showMessageBox( "Cynthia Johnson", "I couldn't save the user file!\n\nData has therefore not been saved", "error", true )
+       hate.window.showMessageBox( "Cynthia Johnson", "I couldn't save the user file!\n\nData has therefore not been saved", "error", true )
     end
 end
