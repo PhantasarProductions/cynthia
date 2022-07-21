0ad3fb62daca4af1b4c49aa681e6b75a299f0f36
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-07-21 18:40:20 +0200

    Closed #82

--- Script/loadpuzzle.lua
+++ Script/loadpuzzle.lua
@@ -21,37 +21,17 @@
 -- Please note that some references to data like pictures or audio, do not automatically
 -- fall under this licenses. Mostly this is noted in the respective files.
 -- 
--- Version: 22.07.17
+-- Version: 22.07.21
 -- </License Block>
---[[
-	Cynthia Johnson
-	Puzzle Loader
-	
-	
-	
-	(c) Jeroen P. Broks, 2017, All rights reserved
-	
-		This program is free software: you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-		(at your option) any later version.
-		
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
-		You should have received a copy of the GNU General Public License
-		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-		
-	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
-	to the project the exceptions are needed for.
-Version: 17.11.12
-]]
+
 
 puzzles = j_hate_import("SCRIPT/PUZZLES/TITLES.LUA")
 
 local function loadpuzzle(f)
-      local ret = j_hate_import("SCRIPT/PUZZLES/MAPS/"..upper(f)..".lua")
+      --local ret = j_hate_import("SCRIPT/PUZZLES/MAPS/"..upper(f)..".lua")
+      local scr = Neil.Globals.JCR6.LoadString("SCRIPT/PUZZLES/MAPS/"..upper(f)..".lua")
+      local fun = load(scr)
+      local ret = fun()
       assert(ret,"Loading map code for: "..f.." failed!")
       ret.script = {}
       local sf = "SCRIPT/PUZZLES/Script/"..upper(f)..".lua"
