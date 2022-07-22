28630b6fa9982be2e751a99aa9f9c855f2e179d6
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-07-22 13:41:16 +0200

    I hate you

--- Script/chain_sections.lhl/ChooseUser.lua
+++ Script/chain_sections.lhl/ChooseUser.lua
@@ -21,32 +21,9 @@
 -- Please note that some references to data like pictures or audio, do not automatically
 -- fall under this licenses. Mostly this is noted in the respective files.
 -- 
--- Version: 22.07.17
+-- Version: 22.07.22
 -- </License Block>
---[[
-	Cynthia Johnson
-	
-	
-	
-	
-	(c) Jeroen P. Broks, 2018, All rights reserved
-	
-		This program is free software: you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-		(at your option) any later version.
-		
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
-		You should have received a copy of the GNU General Public License
-		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-		
-	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
-	to the project the exceptions are needed for.
-Version: 18.01.24
-]]
+
 local cu = {}
 
 
@@ -88,7 +65,7 @@ cu.gui = {
                           login(cu.chosenname)
                           chain.go("MAINMENU")
                         else  
-                          love.window.showMessageBox( "Cynthia Johnson", "ERROR!\n\nSay what? Login mr. nobody?\nPlease pick a name before you click this button!","error")
+                          hate.window.showMessageBox( "Cynthia Johnson", "ERROR!\n\nSay what? Login mr. nobody?\nPlease pick a name before you click this button!","error")
                         end  
                       end                     
                   },
@@ -132,14 +109,14 @@ cu.gui = {
                         -- remove user code 
                         if cu.chosenname then
                           if user.username==cu.chosenname then
-                            love.window.showMessageBox( "Cynthia Johnson", "ERROR!\n\nSorry! I cannot delete you!\nLogin to another accoount in order to get this file deleted.","error")
+                            hate.window.showMessageBox( "Cynthia Johnson", "ERROR!\n\nSorry! I cannot delete you!\nLogin to another accoount in order to get this file deleted.","error")
                           else
-                            love.filesystem.remove("users/"..cu.chosenname..".lua")
-                            love.window.showMessageBox( "Cynthia Johnson", "R.I.P.\n"..cu.chosenname,"info")
+                            hate.filesystem.remove("users/"..cu.chosenname..".lua")
+                            hate.window.showMessageBox( "Cynthia Johnson", "R.I.P.\n"..cu.chosenname,"info")
                             chain.go("CHOOSEUSER")
                           end
                         else  
-                          love.window.showMessageBox( "Cynthia Johnson", "ERROR!\n\nSay what? Remove mr. nobody?\nPlease pick a name before you click this button!")
+                          hate.window.showMessageBox( "Cynthia Johnson", "ERROR!\n\nSay what? Remove mr. nobody?\nPlease pick a name before you click this button!")
                         end  
 
                       end
@@ -170,7 +147,7 @@ print("luna update: choose user done!")
 function cu.arrive()
   cu.UserButtons={}
   cu.chosenname = nil
-  local d = love.filesystem.getDirectoryItems("users")
+  local d = hate.filesystem.getDirectoryItems("users")
   local cnt = 0
   local toomuch = 0
   for g in each(cu.users) do g.visible=false g.checked=false end
@@ -189,7 +166,7 @@ function cu.arrive()
     end
   end
   if toomuch>0 then
-    love.window.showMessageBox( "Cynthia Johnson", "ERROR!\n\nThere were too many user files found.\nThe system only supports "..#cu.users.." and "..#d.." where found meaning I had to ignore "..toomuch.." files", "error", true )
+    hate.window.showMessageBox( "Cynthia Johnson", "ERROR!\n\nThere were too many user files found.\nThe system only supports "..#cu.users.." and "..#d.." where found meaning I had to ignore "..toomuch.." files", "error", true )
   end  
 end
 
